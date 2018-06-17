c5b3c35b309adcde1a166cf147cbc5ee58f70e97
Infauto <40276021+Infauto@users.noreply.github.com>
2018-06-17 23:20:02 +0200

    Create Magie.h

---
+++ Magie.h
@@ -0,0 +1,34 @@
+template <typename Emetteur, typename Recepteur>
+float Brasier (Emetteur emetteur, Recepteur recepteur)
+{
+	float valeur = 4;
+	int cout = valeur;
+	if (emetteur.getMana() >= 0)
+		
+	
+	return valeur;
+}
+template <typename Emetteur, typename Recepteur>
+float glacier (Emetteur emetteur, Recepteur recepteur)
+{
+	float valeur = 4;
+	
+	return valeur;
+}
+
+template <typename Emetteur, typename Recepteur>
+float foudre (Emetteur emetteur, Recepteur recepteur)
+{
+	float valeur = 4;
+	
+	return valeur;
+}
+
+template <typename Emetteur, typename Recepteur>
+float H2O (Emetteur emetteur, Recepteur recepteur)
+{
+	float valeur = 4;
+	
+	return valeur;
+}
+
